bffbf55cb23ce31ae0cc811fa5621c4ef1863d86
Erhan Kurnaz <erhan@globalscreen.nl>
2021-01-13 22:06:43 +0100

    added more commands

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const tmi = require('tmi.js');
+const { randomNumber, eightBallResponses } = require('./utils');
 
 const { BOT_USERNAME, CHANNEL_NAME, OAUTH_TOKEN } = process.env;
 
@@ -32,13 +33,28 @@ const commands = {
         if (msg.length > 1 && !isNaN(msg[1])) {
             const max = Math.floor(Number(msg[1]));
             if (max > 0) {
-                client.say(channel, `@${context['display-name']} rolled: ${Math.floor(Math.random() * max) + 1}`);
+                client.say(channel, `@${context['display-name']} rolled: ${randomNumber(max) + 1}`);
             } else {
                 client.say(channel, `@${context['display-name']} i tried to roll ${max}, but i don't know how to roll negative numbers`)
             }
         } else {
             client.say(channel, `You need to enter a number @${context['display-name']}`)
         }
+    },
+    'pp': (channel, context) => {
+        let size;
+        if (context.username.length < 8) {
+            size = 'you have a small pp, don\'t be embarrassed 🤖';
+        } else if (context.username.length < 12) {
+            size = 'you have an average pp, what an achievement 👏';
+        } else {
+            size = '🎉 you have an HUUUGGGEEE pp, congratulations 🎉';
+        }
+
+        client.say(channel, `@${context.username} ${size}`);
+    },
+    '8ball': (channel, context) => {
+        client.say(channel, `@${context['display-name']} The magic eight ball says: ${eightBallResponses[randomNumber(eightBallResponses.length)]}`);
     }
 };
 
@@ -47,18 +63,22 @@ client.on('message', (channel, context, msg, self) => {
     const commandName = msg.trim();
     if (!self, commandName[0] === '!') {
         // get rid of the ! and trim it to the first word
-        let spaceIndex = commandName.indexOf(' ');
-        let parsedCommand;
-        if (spaceIndex === -1) {
-            parsedCommand = [commandName.substring(1)];
-        } else {
-            parsedCommand = [commandName.substring(1, spaceIndex), commandName.substring(spaceIndex)];
-        }
-        if (commands.hasOwnProperty(parsedCommand[0])) {
-            commands[parsedCommand[0]](channel, context, parsedCommand, self);
-            console.log(`parsed command: ${commandName}`);
-        } else {
-            console.log(`Invalid command ${commandName}`);
+        try {
+            let spaceIndex = commandName.indexOf(' ');
+            let parsedCommand;
+            if (spaceIndex === -1) {
+                parsedCommand = [commandName.substring(1)];
+            } else {
+                parsedCommand = [commandName.substring(1, spaceIndex), commandName.substring(spaceIndex)];
+            }
+            if (commands.hasOwnProperty(parsedCommand[0])) {
+                commands[parsedCommand[0]](channel, context, parsedCommand, self);
+                console.log(`parsed command: ${commandName}`);
+            } else {
+                console.log(`Invalid command ${commandName}`);
+            }
+        } catch (e) {
+            console.error(e);
         }
     }
 });
