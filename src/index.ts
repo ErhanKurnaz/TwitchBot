a11b1a599f9d64f5e4125cf22384fd7638edd085
Erhan Kurnaz <erhan@globalscreen.nl>
2021-08-17 22:21:26 +0200

    added more event listeners

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,31 @@ client.on('connected', () => {
     console.log('bot connected')
 })
 
+client.on('giftpaidupgrade', (channel, username) => {
+    console.log(`${username} has continued gift sub to channel ${channel}`)
+    client.say(channel, `Thank you @${username} for continuing your gift sup`)
+})
+
+client.on('raided', (channel, username, viewers) => {
+    console.log(`${username} raided ${channel} with ${viewers} viewers`)
+    client.say(channel, `Thank you ${username} for RADING WITH ${viewers} VIEWERS!!! Man imagine if this stream was actually entertaining Kappa`)
+})
+
+client.on('resub', (channel, username, monthStreak, message, userstate) => {
+    console.log(`${username} resubbed to ${channel} for ${monthStreak} saying ${message}`)
+    if (!userstate['msg-param-should-share-streak']) {
+        client.say(channel, 'Looks like someone has resubbed *whispers* you know who you are *nuzzles* *gets sprayed with water*')
+        return
+    }
+
+    const cumulativeMonths = ~~userstate['msg-param-cumulative-months']
+    client.say(channel, `Thank you ${username} for resubscribing for ${cumulativeMonths}!!!! I would kiss you but I don't have lips, how bout a little hug instead matsdoGhostHug`)
+})
+
+client.on('subscription', (channel, username, _, message) => {
+    console.log(`${username} has subscribed to ${channel} saying ${message}`)
+    client.say(channel, `Ayyyyyyy ma humanoid @${username} out here with the subscription... that might have been the worst mistake of your life. Enjoy Kappa`)
+})
+
 // Connect to Twitch:
 client.connect()
