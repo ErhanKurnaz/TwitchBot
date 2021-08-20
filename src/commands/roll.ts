06daddcfaf1817d8fc90b62303f0b3f2058339c5
PingPianoPong <wangka.senpai.6@gmail.com>
2021-08-20 22:05:36 +0100

    Removed changes

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -6,7 +6,7 @@ export const description = 'feeling lucky? use this command to roll a N sided di
 export default ({ channel, context, args }: ICommandProps) => {
     if (args.length && !Number.isNaN(args[0])) {
         const max = Math.floor(Number(args[0]))
-        if (max >= 0) {
+        if (max > 0) {
             client.say(channel, `@${context['display-name']} rolled: ${randomNumber(max) + 1}`)
         } else {
             client.say(
