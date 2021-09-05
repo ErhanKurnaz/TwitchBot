88be4250752b201b335da11b62792e970bbe2eba
PingPianoPong <wangka.senpai.6@gmail.com>
2021-09-05 14:05:10 +0100

    Added cooldown + few edits

--- src/commands/lurk.ts
+++ src/commands/lurk.ts
@@ -4,5 +4,5 @@ import { client } from '../index'
 export const description =
     'Use this command to show that you will be lurking. Imagine being rude enough to not be active in chat Kappa'
 export default({ channel, context }: ICommandProps) => {
-    client.say(channel, `@${context['display-name']} is now lurking NotLikeThis Hope to see u soon KonCha`)
+    client.say(channel, `@${context['display-name']} is now lurking. Hope to see u soon KonCha`)
 }
